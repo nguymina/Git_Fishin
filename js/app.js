e832a0847a40cc65a258037e4c96dc37c62d971d
nguymina <mnguyen27639@gmail.com>
2022-07-16 11:12:09 -0600

    figured out why I was making things weird

---
+++ js/app.js
@@ -0,0 +1,37 @@
+/*
+Pseudocode Brainstorm
+
+To do:
+Create player that can be named
+    score starts at 0
+
+Create function for water and fish
+*/
+
+class Player {
+    constructor(name) {
+        this.name = name;
+        this.score = 0;
+        this.rod = "Old Rod";
+    }
+};
+
+
+
+// Random number generator
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+};
+
+// Water + fish generation
+
+let waterPlacement = document.getElementById('water');
+console.log(waterPlacement);
+console.log("connected");
+
+//Make a dom creation that generates 36 water tiles and each tile gets individual id
+
+const createWater = () => {
+    document.createElement("img");
+}
